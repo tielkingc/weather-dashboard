c2797e3fd0f816c453bc38e891603ce4d4660681
tielkingc <tielking5@gmail.com>
2021-05-18 18:38:30 -0400

    user can change city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
-fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=indianapolis&units=imperial&appid=03150e865e408eec25cb79def73567fc'
-  )
-    // Convert the response to JSON
-    .then(function(response) {
-      console.log(response.json());
-    });
+$(".city-search").on("click", function() {
+    var city = document.querySelector(".city").value;
+
+    fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q='+ city + '&units=imperial&appid=03150e865e408eec25cb79def73567fc'
+    )
+        // Convert the response to JSON
+        .then(function(response) {
+        console.log(response.json());
+        });
+})
+
